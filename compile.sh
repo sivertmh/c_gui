d34ac891ed42899cb95cc9d26cc6c5787325736c
Sivert <sivert.mathisen.hansen@gmail.com>
2025-01-27 21:07:08 +0100

    main.c excersices

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "compiling c file to executable..."
+
+sleep 0.5
+
+clear
+
+rm myprog && gcc -o myprog main.c
--- main.c
+++ main.c
@@ -1,11 +1,79 @@
+
+// ARITHMETIC
+
+// #include <stdio.h>
+
+// void main (void) {
+//     int a = 2;
+//     int b = 3;
+//     int c;
+
+//     c = a + b;
+
+//     printf("%d\n", c);
+// }
+
+// CONDITIONALS
+
+// #include <stdio.h>
+// void main (void)
+// {
+//  int a = 1;
+//  if (a == 0)
+//  {
+//  printf ("a is equal to 0\n");
+//  }
+//  else
+//  {
+//  printf ("a is not equal to 0\n");
+//  }
+// }
+
+// WHILE-LOOP
+
+// #include <stdio.h>
+
+// void main (void) {
+//     int a = 1;
+
+//     while (a < 5) {
+//         printf("a is now %d\n", a);
+//         a++;
+//     }
+// }
+
+// A DO-WHILE-LOOP
+
+// #include <stdio.h>
+
+// void main(void) {
+//     int a = 0;
+
+//     do 
+//     {
+//         printf("a is currently %d\n", a);
+//         a = a + 1; // this is the same a a++;
+//     } while(a < 7);
+//         printf("a is now %d and the loop is completed\n", a);
+// }
+
+// A FOR-LOOP WITH "ADVANCED FLOW CONTROL"
+
+// #include <stdio.h>
+
+// void main() 
+// {
+//     int a;
+
+//     for (a = 0; a < 5; a++)
+//     {
+//         printf("a is equal to %d\n", a);
+//     }
+//     printf("a is now %d and the loop is finished\n", a);
+// }
+
+
+
 #include <stdio.h>
 
-void main (void) {
-    int a = 3;
-    int b = 7;
-    int c;
 
-    c = a + b;
-
-    printf("%d\n", c);
-}
