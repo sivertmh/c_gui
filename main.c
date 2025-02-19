15c9c759947f8aa8d6fe298c85d3d3f2ea2e57eb
Sivert <sivert.mathisen.hansen@gmail.com>
2025-02-19 16:56:51 +0100

    made the single-lined comments multi-line.

--- main.c
+++ main.c
@@ -1,79 +1,120 @@
+/*
 
 // ARITHMETIC
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// void main (void) {
-//     int a = 2;
-//     int b = 3;
-//     int c;
+void main (void) {
+    int a = 2;
+    int b = 3;
+    int c;
 
-//     c = a + b;
+    c = a + b;
 
-//     printf("%d\n", c);
-// }
+    printf("%d\n", c);
+}
 
 // CONDITIONALS
 
-// #include <stdio.h>
-// void main (void)
-// {
-//  int a = 1;
-//  if (a == 0)
-//  {
-//  printf ("a is equal to 0\n");
-//  }
-//  else
-//  {
-//  printf ("a is not equal to 0\n");
-//  }
-// }
+#include <stdio.h>
+void main (void)
+{
+ int a = 1;
+ if (a == 0)
+ {
+ printf ("a is equal to 0\n");
+ }
+ else
+ {
+ printf ("a is not equal to 0\n");
+ }
+}
 
 // WHILE-LOOP
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// void main (void) {
-//     int a = 1;
+void main (void) {
+    int a = 1;
 
-//     while (a < 5) {
-//         printf("a is now %d\n", a);
-//         a++;
-//     }
-// }
+    while (a < 5) {
+        printf("a is now %d\n", a);
+        a++;
+    }
+}
 
 // A DO-WHILE-LOOP
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// void main(void) {
-//     int a = 0;
+void main(void) {
+    int a = 0;
 
-//     do 
-//     {
-//         printf("a is currently %d\n", a);
-//         a = a + 1; // this is the same a a++;
-//     } while(a < 7);
-//         printf("a is now %d and the loop is completed\n", a);
-// }
+    do 
+    {
+        printf("a is currently %d\n", a);
+        a = a + 1; // this is the same a a++;
+    } while(a < 7);
+        printf("a is now %d and the loop is completed\n", a);
+}
 
 // A FOR-LOOP WITH "ADVANCED FLOW CONTROL"
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// void main() 
-// {
-//     int a;
+void main() 
+{
+    int a;
 
-//     for (a = 0; a < 5; a++)
-//     {
-//         printf("a is equal to %d\n", a);
-//     }
-//     printf("a is now %d and the loop is finished\n", a);
-// }
+    for (a = 0; a < 5; a++)
+    {
+        printf("a is equal to %d\n", a);
+    }
+    printf("a is now %d and the loop is finished\n", a);
+}
 
+// SWITCH STATEMENT
 
+ #include <stdio.h>
+ void main (void)
+ {
+  unsigned int a = 1;
+
+  switch (a)
+  {
+  case 0 : printf ("a is equal to 0\n");
+  break;
+  case 1 : printf ("a is equal to 1\n");
+  break;
+  default : printf ("a is greater than 1\n");
+  }
+ }
+
+// THE SAME SWITCH STATEMENT, BUT WITH AN IF STATEMENT
+
+ #include <stdio.h>
+ void main (void)
+ {
+  unsigned int a = 0;
+  if (a == 0)
+  {
+  printf ("a is equal to 0\n");
+  }
+  else if (a == 1)
+  {
+  printf ("a is equal to 1\n");
+  }
+  else
+  {
+  printf ("a is greater than 1\n");
+}
+}
+
+*/
 
 #include <stdio.h>
 
-
+void main (void)
+{
+    
+}
